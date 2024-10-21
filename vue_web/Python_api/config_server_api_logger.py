6316b7d742896ddc3338faa912720d6ebdcc0a1d
小重山 <wcg14231022@gmail.com>
2024-10-22 04:35:18 +0800

    日志文件按日期分割

--- vue_web/Python_api/config_server_api_logger.py
+++ vue_web/Python_api/config_server_api_logger.py
@@ -1,6 +1,7 @@
 import logging
 import os.path
 import sys
+from logging.handlers import TimedRotatingFileHandler
 
 # 创建全局日志器
 logger = logging.getLogger(__name__)
@@ -38,3 +39,12 @@ socketio_handler.setFormatter(formatter)
 
 # 将 SocketIOHandler 添加到日志器
 logger.addHandler(socketio_handler)
+
+# 创建文件处理器，按时间分割日志文件
+log_file_path = os.path.join(os.path.dirname(__file__), 'config_server.log')
+file_handler = TimedRotatingFileHandler(log_file_path, when='midnight', interval=1, backupCount=7)
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+# 将文件处理器添加到日志器
+logger.addHandler(file_handler)
