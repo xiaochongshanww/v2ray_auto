61035fb54d4ad64078c704613966878e280c2ee2
小重山 <wcg14231022@gmail.com>
2026-06-10 10:55:15 +0800

    refactor: add package root

---
+++ v2ray_auto/__init__.py
@@ -0,0 +1,7 @@
+"""v2ray_auto package.
+
+This branch is a destructive rewrite. Public interfaces are no longer compatible
+with the historical scripts in the repository root.
+"""
+
+__version__ = "0.2.0-refactor"
