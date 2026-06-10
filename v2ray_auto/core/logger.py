93200611fbd1aa100e2fbe507f2968ae214bf2e6
小重山 <wcg14231022@gmail.com>
2026-06-10 11:09:01 +0800

    refactor: add logger helper

---
+++ v2ray_auto/core/logger.py
@@ -0,0 +1,15 @@
+"""Project logger helpers."""
+
+from __future__ import annotations
+
+import logging
+
+
+def get_logger(name: str = "v2ray_auto", level: str = "INFO") -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(getattr(logging, level.upper(), logging.INFO))
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(message)s"))
+        logger.addHandler(handler)
+    return logger
